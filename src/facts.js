3714f95e169459922a12f1e6f2949edd62fe4c52
Luzi82 <luzi82@gmail.com>
2017-07-15 17:39:44 +0800

    10 facts

--- src/facts.js
+++ src/facts.js
@@ -4,9 +4,13 @@ module.exports = {
     "FACTS_EN": [
         "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
         "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
+        "In 1993, Schmidhuber's neural history compressor implemented as an unsupervised stack of recurrent neural networks  solved a \"Very Deep Learning\" task that required more than 1000 layers in an RNN unfolded in time.",
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "Apple inc. release Siri in 2011.  The assistant uses voice queries and a natural language user interface to answer questions, make recommendations, and perform actions by delegating requests to a set of Internet services.",
+        "In July 2013, Vislab demonstrated BRAiVE, a vehicle that moved autonomously on a mixed traffic route open to public traffic.",
+        "Microsoft Deep Residual Networks won the ImageNet Large Scale Visual Recognition Challenge in 2015.  The network involve 152 layers and achieve 3.57% error rate.",
+        "In 2016, a go-playing program named AlphaGo, developed by Google DeepMind, beat a 9-dan professional Lee Sedol without handicaps.",
     ]
 }
